889d182e6be0c1da2522c6edfd7f5968721ff1c1
aurora0x27 <qingfengzl0831@163.com>
2025-04-17 17:24:11 +0800

    add wakatime plugin

---
+++ lua/default/plugins/wakatime.lua
@@ -0,0 +1,7 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+return {
+    'wakatime/vim-wakatime',
+    -- lazy = false ,
+    event = { 'BufReadPost', 'BufNewFile' },
+}
