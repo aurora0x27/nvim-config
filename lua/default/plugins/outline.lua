189d7d51e6bdb3cc28a853e446c6ab6c96237074
aurora0x27 <qingfengzl0831@163.com>
2025-05-25 18:48:07 +0800

    Add outline plugin

---
+++ lua/default/plugins/outline.lua
@@ -0,0 +1,17 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+return {
+    'Bekaboo/dropbar.nvim',
+    -- optional, but required for fuzzy finder support
+    event = { 'BufReadPost', 'BufNewFile' },
+    dependencies = {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make',
+    },
+    config = function()
+        local dropbar_api = require 'dropbar.api'
+        vim.keymap.set('n', '<Leader>fs', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
+        vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
+        vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
+    end,
+}
