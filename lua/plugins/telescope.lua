3cb829c86a95cf3edc57b8a89ab0c6f9ddc3d2dd
aurora0x27 <qingfengzl0831@163.com>
2025-08-07 16:20:04 +0800

    Enable focus on telescope preview

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,20 @@
 
 -- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
+local focus_preview = function(prompt_bufnr)
+    local action_state = require 'telescope.actions.state'
+    local picker = action_state.get_current_picker(prompt_bufnr)
+    local prompt_win = picker.prompt_win
+    local previewer = picker.previewer
+    local bufnr = previewer.state.bufnr or previewer.state.termopen_bufnr
+    local winid = previewer.state.winid or vim.fn.win_findbuf(bufnr)[1]
+    vim.keymap.set({ 'n', 'i' }, '<C-l>', function()
+        vim.cmd(string.format('noautocmd lua vim.api.nvim_set_current_win(%s)', prompt_win))
+    end, { buffer = bufnr })
+    vim.cmd(string.format('noautocmd lua vim.api.nvim_set_current_win(%s)', winid))
+    -- api.nvim_set_current_win(winid)
+end
+
 local Telescope = {
     'nvim-telescope/telescope.nvim',
     opts = {
@@ -27,6 +41,14 @@ local Telescope = {
                 '%.cxx',
                 '%.cmake',
                 'Online.*%.xml',
+            },
+            mappings = {
+                n = {
+                    ['<C-l>'] = focus_preview,
+                },
+                i = {
+                    ['<C-l>'] = focus_preview,
+                },
             },
         },
     },
