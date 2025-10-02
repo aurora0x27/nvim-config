8893e0058a7c0d991d82b6e87ea1ad1d1a4ffde2
aurora0x27 <qingfengzl0831@163.com>
2025-10-02 21:03:11 +0800

    Use shorter cwd

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -136,6 +136,30 @@ local StatusLine = {
             end,
         }
 
+        local function shorten_cwd(dir)
+            local parts = {}
+            for p in string.gmatch(dir, '[^/]+') do
+                table.insert(parts, p)
+            end
+
+            local prefix
+            local remaining_parts
+            if vim.startswith(dir, '~') then
+                prefix = '~'
+                remaining_parts = { unpack(parts, 2) }
+            else
+                prefix = '/' .. parts[1]
+                remaining_parts = parts
+            end
+
+            if #remaining_parts > 5 then
+                local last4 = { unpack(remaining_parts, #remaining_parts - 3, #remaining_parts) }
+                return prefix .. '/…' .. '/' .. table.concat(last4, '/')
+            else
+                return dir
+            end
+        end
+
         local WorkDir = {
             init = function(self)
                 local cwd = vim.fn.getcwd(0)
@@ -143,7 +167,7 @@ local StatusLine = {
                 self.mode = vim.fn.mode()
             end,
             provider = function(self)
-                return '   ' .. self.cwd .. ' '
+                return '   ' .. shorten_cwd(self.cwd) .. ' '
             end,
             hl = function(self)
                 local mode_hl = {
