a4ba2b6d0e5ce412bcfe8a9d823a581e12777cf5
aurora0x27 <qingfengzl0831@163.com>
2025-10-14 16:47:59 +0800

    Alias ft zsh -> sh

--- lua/plugins/blinkcmp.lua
+++ lua/plugins/blinkcmp.lua
@@ -89,6 +89,7 @@ local CodeCompletion = {
                         extended_filetypes = {
                             astro = { 'html' },
                             markdown = { 'blog', 'html' },
+                            zsh = { 'sh' },
                         },
                     },
                 },
