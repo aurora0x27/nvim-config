0ad2123a37b9af2408d2f30826e168a9b9dcd795
aurora0x27 <qingfengzl0831@163.com>
2025-10-19 21:59:55 +0800

    Misc patch

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -3,8 +3,6 @@
 local AutoCmd = {}
 
 function AutoCmd.apply()
-    local mocha = require('catppuccin.palettes').get_palette 'mocha'
-
     -- Highlight yanked text
     vim.api.nvim_create_autocmd('TextYankPost', {
         pattern = '*',
@@ -19,12 +17,6 @@ function AutoCmd.apply()
     -- Do not display warnings
     ---@diagnostic disable: duplicate-set-field
     vim.deprecate = function() end
-
-    -- set Blink border highlight
-    vim.api.nvim_set_hl(0, 'BlinkCmpMenuBorder', { fg = mocha.blue })
-    vim.api.nvim_set_hl(0, 'BlinkCmpDocBorder', { fg = mocha.blue })
-    vim.api.nvim_set_hl(0, 'BlinkCmpSignatureHelpBorder', { fg = mocha.blue })
-    vim.api.nvim_set_hl(0, 'BlinkCmpDocSeparator', { fg = mocha.blue })
 
     vim.api.nvim_create_autocmd('BufReadPost', {
         callback = function()
@@ -41,18 +33,6 @@ function AutoCmd.apply()
         pattern = 'help',
         callback = function()
             vim.cmd 'wincmd T'
-        end,
-    })
-
-    -- '*.tmpl' template file in configuration
-    vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
-        pattern = '*.tmpl',
-        callback = function(args)
-            local fname = vim.fn.fnamemodify(args.file, ':t')
-            local m = fname:match '%.([%w_]+)%.tmpl$'
-            if m then
-                vim.bo[args.buf].filetype = vim.filetype.match { filename = 'dummy.' .. m } or m
-            end
         end,
     })
 end
