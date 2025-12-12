72749074cc5f6632ca1cccf372034708bbc4439d
aurora0x27 <qingfengzl0831@163.com>
2025-12-12 23:40:52 +0800

    Use nvim-highlight-colors instead of nvim-colorizer

--- lua/plugins/color-preview.lua
+++ lua/plugins/color-preview.lua
@@ -3,21 +3,75 @@
 -- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 local ColorPreview = {
-    'norcalli/nvim-colorizer.lua',
+    'brenoprata10/nvim-highlight-colors',
     event = { 'BufReadPost', 'BufNewFile' },
-    config = function()
-        require('colorizer').setup({ '*' }, {
-            RGB = false, -- #RGB hex codes `#EAF`
-            RRGGBB = true, -- #RRGGBB hex codes `#39C5BB`, `#66ccff`
-            names = false, -- "Name" codes like `Blue`
-            RRGGBBAA = true, -- #RRGGBBAA hex codes `#66ccffff`
-            rgb_fn = true, -- CSS rgb() and rgba() functions
-            hsl_fn = false, -- CSS hsl() and hsla() functions
-            css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-            css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-            -- Available modes: foreground, background
-            mode = 'background', -- Set the display mode.
-        })
+    init = function()
+        vim.opt.termguicolors = true
+    end,
+    opts = {
+        ---Render style
+        ---@usage 'background'|'foreground'|'virtual'
+        render = 'background',
+
+        ---Set virtual symbol (requires render to be set to 'virtual')
+        virtual_symbol = '■',
+
+        ---Set virtual symbol suffix (defaults to '')
+        virtual_symbol_prefix = '',
+
+        ---Set virtual symbol suffix (defaults to ' ')
+        virtual_symbol_suffix = ' ',
+
+        ---Set virtual symbol position()
+        ---@usage 'inline'|'eol'|'eow'
+        ---inline mimics VS Code style
+        ---eol stands for `end of column` - Recommended to set `virtual_symbol_suffix = ''` when used.
+        ---eow stands for `end of word` - Recommended to set `virtual_symbol_prefix = ' ' and virtual_symbol_suffix = ''` when used.
+        virtual_symbol_position = 'inline',
+
+        ---Highlight hex colors, e.g. '#FFFFFF'
+        enable_hex = true,
+
+        ---Highlight short hex colors e.g. '#fff'
+        enable_short_hex = true,
+
+        ---Highlight rgb colors, e.g. 'rgb(0 0 0)'
+        enable_rgb = true,
+
+        ---Highlight hsl colors, e.g. 'hsl(150deg 30% 40%)'
+        enable_hsl = true,
+
+        ---Highlight ansi colors, e.g '\033[0;34m'
+        enable_ansi = true,
+
+        -- Highlight hsl colors without function, e.g. '--foreground: 0 69% 69%;'
+        enable_hsl_without_function = true,
+
+        ---Highlight CSS variables, e.g. 'var(--testing-color)'
+        enable_var_usage = true,
+
+        ---Highlight named colors, e.g. 'green'
+        enable_named_colors = false,
+
+        ---Highlight tailwind colors, e.g. 'bg-blue-500'
+        enable_tailwind = false,
+
+        ---Set custom colors
+        ---Label must be properly escaped with '%' to adhere to `string.gmatch`
+        --- :help string.gmatch
+        custom_colors = {
+            { label = '%-%-theme%-primary%-color', color = '#0f0219' },
+            { label = '%-%-theme%-secondary%-color', color = '#5a5d64' },
+        },
+
+        -- Exclude filetypes or buftypes from highlighting e.g. 'exclude_buftypes = {'text'}'
+        exclude_filetypes = {},
+        exclude_buftypes = {},
+        -- Exclude buffer from highlighting e.g. 'exclude_buffer = function(bufnr) return vim.fn.getfsize(vim.api.nvim_buf_get_name(bufnr)) > 1000000 end'
+        exclude_buffer = function(bufnr) end,
+    },
+    config = function(_, opts)
+        require('nvim-highlight-colors').setup(opts)
     end,
 }
 
