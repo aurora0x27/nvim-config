354a598bfbc4e4decf73882641c8234b8604dcfa
aurora0x27 <qingfengzl0831@163.com>
2026-01-05 13:02:21 +0800

    Optimize overseer template provider

--- lua/config/overseer/template/pre-commit.lua
+++ lua/config/overseer/template/pre-commit.lua
@@ -1,35 +1,37 @@
+local function get_precommit_cfg(opts)
+    return vim.fs.find({ '.pre-commit-config.yaml', '.pre-commit-config.yml' }, { upward = true, path = opts.dir })[1]
+end
+
+---@type overseer.TemplateFileProvider
 return {
-    name = 'RunPreCommitTmpl',
-    generator = function()
-        local cond = function()
-            local has_cfg = vim.fn.filereadable(vim.fn.getcwd() .. '/.pre-commit-config.yaml') == 1
-                or vim.fn.filereadable(vim.fn.getcwd() .. '/.pre-commit-config.yml') == 1
-            local has_binary = vim.fn.executable 'pre-commit'
-            if has_binary and has_cfg then
-                return true
-            elseif not has_binary then
-                return false, 'Binary `pre-commit` not found'
-            end
-            return false
+    name = 'RunPrecommitHook',
+
+    cache_key = function(opts)
+        return get_precommit_cfg(opts)
+    end,
+
+    generator = function(opts, cb)
+        if vim.fn.executable 'pre-commit' == 0 then
+            return cb 'Binary `pre-commit` not found'
         end
 
-        local ok, msg = cond()
-        if ok then
-            local tmpl = {
-                name = 'run precommit hook',
+        local cfg = get_precommit_cfg(opts)
+        if not cfg then
+            return cb 'No pre-commit config found'
+        end
+
+        local cwd = vim.fs.dirname(cfg)
+
+        cb {
+            {
+                name = 'pre-commit run --all-files',
                 builder = function()
                     return {
                         cmd = { 'pre-commit', 'run', '--all-files' },
-                        name = 'Run precommit',
-                        cwd = vim.fn.getcwd(),
+                        cwd = cwd,
                     }
                 end,
-            }
-            -- IMPORTANT: must return a list of templates
-            return { tmpl }
-        else
-            -- correct: return string for error, or nil
-            return msg or nil
-        end
+            },
+        }
     end,
 }
