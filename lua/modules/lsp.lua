20d2f052f5921fd8e91d4a9d8f66b31c42cc8585
aurora0x27 <qingfengzl0831@163.com>
2026-04-17 23:23:09 +0800

    Simplify lsp

--- lua/modules/lsp.lua
+++ lua/modules/lsp.lua
@@ -5,141 +5,20 @@ local M = {}
 
 local lsp = vim.lsp
 local api = vim.api
-local util = lsp.util
 local methods = lsp.protocol.Methods
-local hover_ns = api.nvim_create_namespace 'hover'
 local misc = require 'utils.misc'
+local thunk = require('utils.loader').thunk
+local bind = require 'utils.loader'.bind
 
 local lsp_list = require('modules.lang').get_lsp_enable_list()
 
 --------------------------------------------------------------------------------
 -- Override lsp.hover
 --------------------------------------------------------------------------------
-local hover = function(config)
-    config = config or {}
-    config.border = config.border or 'rounded'
-    config.focus_id = methods.textDocument_hover
-    config.max_width = config.max_width or 80
-    config.max_height = config.max_height or 20
-
-    lsp.buf_request_all(0, methods.textDocument_hover, function(client)
-        ---@diagnostic disable:param-type-mismatch
-        return util.make_position_params(nil, client.offset_encoding)
-    end, function(results, ctx)
-        local bufnr = assert(ctx.bufnr)
-        if api.nvim_get_current_buf() ~= bufnr then
-            return
-        end
-
-        local results1 = {}
-        for client_id, resp in pairs(results) do
-            local err, result = resp.err, resp.result
-            if err then
-                lsp.log.error(err.code, err.message)
-            elseif result then
-                results1[client_id] = result
-            end
-        end
-
-        if vim.tbl_isempty(results1) then
-            if config.silent ~= true then
-                misc.info('No information available', { title = 'Lsp Hover' })
-            end
-            return
-        end
-
-        local contents = {}
-        local nresults = #vim.tbl_keys(results1)
-        local format = 'markdown'
-
-        for client_id, result in pairs(results1) do
-            local client = assert(lsp.get_client_by_id(client_id))
-            if nresults > 1 then
-                contents[#contents + 1] = string.format('# %s', client.name)
-            end
-
-            if
-                type(result.contents) == 'table'
-                and result.contents.kind == 'plaintext'
-            then
-                if #results1 == 1 then
-                    format = 'plaintext'
-                    contents = vim.split(
-                        result.contents.value or '',
-                        '\n',
-                        { trimempty = true }
-                    )
-                else
-                    contents[#contents + 1] = '```'
-                    vim.list_extend(
-                        contents,
-                        vim.split(
-                            result.contents.value or '',
-                            '\n',
-                            { trimempty = true }
-                        )
-                    )
-                    contents[#contents + 1] = '```'
-                end
-            else
-                vim.list_extend(
-                    contents,
-                    util.convert_input_to_markdown_lines(result.contents)
-                )
-            end
-
-            if result.range then
-                local start = result.range.start
-                local end_ = result.range['end']
-                local start_idx = util._get_line_byte_from_position(
-                    bufnr,
-                    start,
-                    client.offset_encoding
-                )
-                local end_idx = util._get_line_byte_from_position(
-                    bufnr,
-                    end_,
-                    client.offset_encoding
-                )
-                vim.hl.range(
-                    bufnr,
-                    hover_ns,
-                    'LspReferenceTarget',
-                    { start.line, start_idx },
-                    { end_.line, end_idx },
-                    {
-                        priority = vim.hl.priorities.user,
-                    }
-                )
-            end
-
-            contents[#contents + 1] = '---'
-        end
-        contents[#contents] = nil
-
-        if vim.tbl_isempty(contents) then
-            if config.silent ~= true then
-                misc.info('No information available', { title = 'Lsp Hover' })
-            end
-            return
-        end
-
-        local _, winid = util.open_floating_preview(contents, format, config)
-
-        api.nvim_create_autocmd('WinClosed', {
-            pattern = tostring(winid),
-            once = true,
-            callback = function()
-                api.nvim_buf_clear_namespace(bufnr, hover_ns, 0, -1)
-            end,
-        })
-    end)
-end
+local hover_impl = lsp.buf.hover
 
 local function lsp_buf_setup(event)
     local bufnr = event.buf
-    local thunk = require('utils.loader').thunk
-    local bind = require('utils.loader').bind
     local map = vim.keymap.set
 
     ----------------------------------------------------------------------------
@@ -238,7 +117,12 @@ function M.setup()
         lsp.enable(name)
     end
 
-    lsp.buf.hover = hover
+    lsp.buf.hover = bind(hover_impl, {
+        border = 'rounded',
+        focus_id = methods.textDocument_hover,
+        max_width = 80,
+        max_height = 20,
+    })
 
     api.nvim_create_autocmd('LspAttach', {
         group = api.nvim_create_augroup('lsp-attach', { clear = true }),
@@ -271,7 +155,7 @@ function M.setup()
     )
 
     api.nvim_create_user_command('LspLog', function()
-        vim.cmd(string.format('tabnew %s', lsp.get_log_path()))
+        vim.cmd(string.format('tabnew %s', lsp.log.get_filename()))
     end, {
         desc = 'Opens the Nlsp client log.',
     })
