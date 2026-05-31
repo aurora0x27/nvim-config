7db3cb90624b3895fc14746e1ab43c77aa78c6cc
aurora0x27 <qingfengzl0831@163.com>
2026-05-31 14:11:10 +0800

    Use luajit to detect os

--- lua/utils/detect.lua
+++ lua/utils/detect.lua
@@ -2,22 +2,22 @@ local M = {}
 
 ---@return boolean
 function M.is_unix()
-  return vim.fn.has 'unix' == 1
+  return jit.os == 'POSIX' or jit.os == 'BSD' or jit.os == 'OSX'
 end
 
 ---@return boolean
 function M.is_linux()
-  return vim.fn.has 'linux' == 1
+  return jit.os == 'Linux'
 end
 
 ---@return boolean
 function M.is_macos()
-  return vim.fn.has 'mac' == 1
+  return jit.os == 'OSX'
 end
 
 ---@return boolean
 function M.is_windows()
-  return vim.fn.has 'win32' == 1 or vim.fn.has 'win64' == 1
+  return jit.os == 'Windows'
 end
 
 ---@param cmd string
