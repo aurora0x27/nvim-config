208df156d8493321eafc74e476c6e5cea1869a6f
aurora0x27 <qingfengzl0831@163.com>
2026-07-07 16:20:42 +0800

    Add tabclose policy

--- lua/edit/options.lua
+++ lua/edit/options.lua
@@ -27,6 +27,7 @@ local Opt = {
   splitright = true,
   splitbelow = true,
   guicursor = 'n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,t:block-blinkon0-TermCursor',
+  tabclose = 'uselast',
 }
 
 local misc = require 'utils.misc'
